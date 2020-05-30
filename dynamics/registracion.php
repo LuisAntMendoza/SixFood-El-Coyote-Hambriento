13be74c0f16febebacf7457b7dfe7c6518d6ef07
LuisAntMendoza <mendozaluis@comunidad.unam.mx>
2020-05-30 17:10:06 -0500

    DOCS: se cambió el link de Facebook a la página de SixFood

--- dynamics/registracion.php
+++ dynamics/registracion.php
@@ -247,7 +247,7 @@ echo '
     <section>
         <aside class="redes">
             <h3 class="redes-titulo">¡Síguenos!</h3>
-            <a href="http://www.facebook.com" target="_blank">
+            <a href="https://www.facebook.com/coyo.sixfood.5" target="_blank">
                 <div class="cuadro-red" id="facebook"><img src="../statics/img/logos-red/logo-facebook.png" alt="Logo Facebook" class="logo-red">
                     <h3 class="h3-red">Facebook</h3>
                 </div>
